b8ec4d473a8a02bcf4092dd19c3cab0111114317
NickLombardo26 <54369166+NickLombardo26@users.noreply.github.com>
2020-01-22 20:06:21 -0600

    Update sketch.js

--- p5/Week2/Trees/sketch.js
+++ p5/Week2/Trees/sketch.js
@@ -7,21 +7,31 @@ function setup() {
 background(76, 200, 255);
 noStroke();
 
+//Mntn
 fill(85,90,98);
 arc(650, 400, 280, 500, PI, TWO_PI);
 
+//Cave
 fill('black');
 arc(650, 250, 80, 120, PI, TWO_PI);
 
+//Snowtop
+fill('white');
+arc(650, 200, 160, 100, PI, TWO_PI);
+
+//Ground
 fill('green');
 rect(0, 360, 720, 40);
 
+//Tree trunk
 fill(110,58,11);
 rect(220, 200, 63, 300);
 
+//Sun
 fill('yellow');
 ellipse(0, 10, 72, 72);
 
+//Pine needles
 fill(11,110,58);
 triangle(150, 250, 350, 250, 250, 150);
 
@@ -30,5 +40,5 @@ triangle(100, 300, 400, 300, 250, 200);
 
 fill(0) ;
 textsize(40);
-  text( smouseX + ", " + mouseY, 100, 100);
+  text(mouseX + ", " + mouseY, 100, 100);
 }
