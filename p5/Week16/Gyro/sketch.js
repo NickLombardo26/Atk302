611ba803890a1cfbebc97f7460f54087a809ecaf
NickLombardo26 <54369166+NickLombardo26@users.noreply.github.com>
2020-05-01 23:06:27 -0500

    Update sketch.js

--- p5/Week16/Gyro/sketch.js
+++ p5/Week16/Gyro/sketch.js
@@ -65,7 +65,7 @@ function draw() {
   // MORE DECORATIONS - write that pretty ATK type on top.
   fill('Green');
   noStroke();
-  textSize(300);
+  textSize(200);
   textAlign(CENTER);
   text("ALIENS", width / 2, height / 2);
 
